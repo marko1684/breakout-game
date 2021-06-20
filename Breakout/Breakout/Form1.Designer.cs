b4e5b02db3bf95da90cbe3492edec4528c3d7c34
Marko Petrovic <70198333+marko1684@users.noreply.github.com>
2021-06-20 17:22:27 +0200

    Add files via upload

---
+++ Breakout/Breakout/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Breakout
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picKockica = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.picPloca = new System.Windows.Forms.PictureBox();
+            this.labelNivo = new System.Windows.Forms.Label();
+            this.labelSkor = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picKockica)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPloca)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picKockica
+            // 
+            this.picKockica.BackColor = System.Drawing.Color.Red;
+            this.picKockica.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picKockica.Location = new System.Drawing.Point(287, 288);
+            this.picKockica.Name = "picKockica";
+            this.picKockica.Size = new System.Drawing.Size(25, 25);
+            this.picKockica.TabIndex = 0;
+            this.picKockica.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 15;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // picPaddle
+            // 
+            this.picPloca.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.picPloca.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picPloca.Location = new System.Drawing.Point(214, 481);
+            this.picPloca.Name = "picPaddle";
+            this.picPloca.Size = new System.Drawing.Size(200, 25);
+            this.picPloca.TabIndex = 1;
+            this.picPloca.TabStop = false;
+            // 
+            // label1
+            // 
+            this.labelNivo.AutoSize = true;
+            this.labelNivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.labelNivo.Location = new System.Drawing.Point(12, 528);
+            this.labelNivo.Name = "label1";
+            this.labelNivo.Size = new System.Drawing.Size(63, 24);
+            this.labelNivo.TabIndex = 2;
+            this.labelNivo.Text = "Nivo 1";
+            // 
+            // label2
+            // 
+            this.labelSkor.AutoSize = true;
+            this.labelSkor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.labelSkor.Location = new System.Drawing.Point(126, 528);
+            this.labelSkor.Name = "label2";
+            this.labelSkor.Size = new System.Drawing.Size(83, 24);
+            this.labelSkor.TabIndex = 3;
+            this.labelSkor.Text = "Bodovi 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.labelSkor);
+            this.Controls.Add(this.labelNivo);
+            this.Controls.Add(this.picPloca);
+            this.Controls.Add(this.picKockica);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.picKockica)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPloca)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picKockica;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox picPloca;
+        private System.Windows.Forms.Label labelNivo;
+        private System.Windows.Forms.Label labelSkor;
+    }
+}
+
---
+++ Breakout/Breakout/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Breakout
+{
+    public partial class Form1 : Form
+    {
+        int vBrzina, hBrzina;
+        int brojRedova = 5;
+        int brojKolona = 6;
+        int skor = 0;
+        int nivo = 1;
+        PictureBox[,] blokovi;
+
+        public Form1()
+        {
+            vBrzina = 2;
+            hBrzina = 2;
+            postaviBlokove();
+            InitializeComponent();
+        }
+
+        //postavljanje blokova za rusenje
+        private void postaviBlokove()
+        {
+            int blokVisina = 25;
+            int blokSirina = 100;
+            blokovi = new PictureBox[brojRedova, brojKolona];
+
+            for(int x = 0; x < brojRedova; x++)
+            {
+                for(int y = 0; y < brojKolona; y++)
+                {
+                    blokovi[x, y] = new PictureBox();
+                    blokovi[x, y].Width = blokSirina;
+                    blokovi[x, y].Height = blokVisina;
+                    blokovi[x, y].Top = blokVisina * x;
+                    blokovi[x, y].Left = blokSirina * y;
+                    blokovi[x, y].BackColor = Color.Green;
+                    blokovi[x, y].BorderStyle = BorderStyle.Fixed3D;
+                    this.Controls.Add(blokovi[x, y]);
+                }
+            }
+        }
+        // Na pomeranje misa po formi pomera se ploca
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            picPloca.Left = e.X- picPloca.Width / 2;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            //dodirivanje kockice i ivica forme
+            picKockica.Top += vBrzina;
+            picKockica.Left += hBrzina;
+            if(picKockica.Bottom > ClientRectangle.Height)
+            {
+                vBrzina = -vBrzina;
+            }
+            if(picKockica.Top < 0)
+            {
+                vBrzina = -vBrzina;
+            }
+            if( picKockica.Right > ClientRectangle.Width)
+            {
+                hBrzina = -hBrzina;
+            }
+            if (picKockica.Left < 0)
+            {
+                hBrzina = -hBrzina;
+            }
+
+            // dodirivanje kockice i ploce
+            if (picKockica.Bounds.IntersectsWith(picPloca.Bounds) == true)
+            {
+                vBrzina = -vBrzina;
+            }
+
+            //dodirivanje kockice i blokova
+
+            for(int x = 0; x < brojRedova; x++)
+            {
+                for(int y = 0; y < brojKolona; y++)
+                {
+                    if(picKockica.Bounds.IntersectsWith(blokovi[x,y].Bounds) && blokovi[x, y].Visible == true)
+                    {
+                        blokovi[x, y].Visible = false;
+                        vBrzina = -vBrzina;
+                        skor += (nivo * 100);
+                        labelSkor.Text = "bodovi" + " " + skor.ToString();
+                    }
+                }
+            }
+
+            // provera levela
+            int brojBlokova = brojKolona * brojRedova;
+            for (int x = 0; x < brojRedova; x++)
+            {
+                for (int y = 0; y < brojKolona; y++)
+                {
+                    if(blokovi[x,y].Visible == false)
+                    {
+                        brojBlokova--;
+                    }
+                }
+                if (brojBlokova == 0)
+                {
+                    string[] s = labelNivo.Text.Split(' ');
+                    nivo = Convert.ToInt32(s[1]) + 1;
+                    labelNivo.Text = s[0] + " " + nivo.ToString();
+
+                    // ponovo se postavljaju kockice
+                    for (int i = 0; i < brojRedova; i++)
+                    {
+                        for (int j = 0; j < brojKolona; j++)
+                        {
+                            blokovi[i, j].Visible = true;
+                        }
+
+                        // kockica se postavlja na sredinu forme i nastavlja se sledeci nivo
+                        picKockica.Top = ClientRectangle.Height / 2;
+                        picKockica.Left = ClientRectangle.Width / 2 - picKockica.Width / 2;
+
+                        // horizontalna i vertikalna brzina kockice se povecavaju za 1
+                        vBrzina++;
+                        hBrzina++;
+                    }
+                }
+            }
+
+            // Game over
+   
+            if(picKockica.Top > picPloca.Top)
+            {       
+                timer1.Stop();
+                MessageBox.Show("GAME OVER!!! \nVas skor je " + skor.ToString());
+            }
+        }
+    }
+}
